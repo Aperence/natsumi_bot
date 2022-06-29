4e478ee0d6ad0aadcad3db0350fc019ed14c7cc5
Aperence <anthony.doeraene@hotmail.com>
2022-06-29 20:09:31 +0200

    added previous option

--- commands/channels.js
+++ commands/channels.js
@@ -5,7 +5,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('channels')
 		.setDescription('List of channels'),
-	async execute(interaction, client) {
+	async execute(interaction, client, reply=true) {
 		
 		var response = client.channels.cache.filter(channel => !channel.parentId).map(channel=>{ return {"label" : channel.name, "value" : channel.id } })
 
@@ -18,9 +18,18 @@ module.exports = {
 				.setMaxValues(response.length)
 				.addOptions(response),
 		);
-		await interaction.reply({ content: 'Channels', components: [row], ephemeral : true });
+		if (reply){
+			await interaction.reply({ content: 'Channels', components: [row], ephemeral : true });
+		}else{
+			await interaction.update({ content: 'Channels', components: [row], ephemeral : true });
+		}
+		
 	},
 	async handleSelection(interaction, client){
+		if (interaction.values.includes("noId")){
+			await this.execute(interaction, client, false)
+			return 
+		}
 		var response = client.channels.cache.filter(channel => interaction.values.includes(channel.parentId))
 											.map(channel=>{ return {"label" : channel.name, "value" : channel.id } })
 		if (response.length === 0){
@@ -28,6 +37,9 @@ module.exports = {
 			await interaction.update({content : interaction.values.map(channelId => client.channels.cache.get(channelId).name).join(", "), components : []})
 			return
 		}
+		var Id = client.channels.cache.get(interaction.values[0]).parentId || "noId"
+		response.splice(0, 0, {"label" : "Previous", "value" : Id});
+		console.log(response)
 		const row = new MessageActionRow()
 		.addComponents(
 			new MessageSelectMenu()
